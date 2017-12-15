d12d7a70b5d8309761665f4454be5b2403d29924
vkumra-broad <vkumra@broadinstitute.org>
2017-12-15 11:19:13 -0600

    change api end point

--- jupyter-docker/Jupyter_Server_Extension.py
+++ jupyter-docker/Jupyter_Server_Extension.py
@@ -15,5 +15,5 @@ def load_jupyter_server_extension(nb_server_app):
     nb_server_app.log.info('Leo jupyter server extension loaded')
     web_app = nb_server_app.web_app
     host_pattern = '.*$'
-    route_pattern = url_path_join(web_app.settings['base_url'], '/hello')
+    route_pattern = url_path_join(web_app.settings['base_url'], '/api/hello')
     web_app.add_handlers(host_pattern, [(route_pattern, HelloWorldHandler)])
